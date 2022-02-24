bd6161553afca95accbc7c8ae59e29df242c729e
byw <wooyb3174@naver.com>
2022-02-24 15:51:51 +0900

    오류 수정

--- src/main/java/git/pjt/woo/NewsController.java
+++ src/main/java/git/pjt/woo/NewsController.java
@@ -1,16 +1,13 @@
 package git.pjt.woo;
 
 public class NewsController {
-<<<<<<< HEAD
+
 	void master222() {
 		
 	}
 	
 	void master() {
-=======
-	void mybranch222() {
->>>>>>> mybranch
-		
+
 	}
 	
 	void aaa() {
